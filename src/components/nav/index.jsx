8a3919be878bc46a0a1bb5fc8d61e8f0927a524f
Pab-Source <pablo.tirado.toledo@gmail.com>
2020-05-20 02:01:17 -0230

    refactor routing

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -9,41 +9,48 @@ import { useDispatch, useSelector } from 'react-redux'
 import { ClearStorageAction } from '../../redux/actions'
 
 const Nav = ({ children }) => {
-	const state = useSelector(state => state)
+	const token = useSelector(state => state.reducerLogin.token)
 	const dispatch = useDispatch()
 	const history = useHistory()
-	const {
-		reducerLogin: { token }
-	} = state
 
 	const logout = () => dispatch(ClearStorageAction())
 
 	useLayoutEffect(() => {
-		if (!token) {
-			history.push('/login')
-		}
+		!token && history.push('/login')
 	}, [token, history])
 
 	return (
 		<>
 			<div className='nav'>
 				<div className='nav__left'>
-					<NavLink activeClassName='nav__link-active' className='nav__link' to='/'>
+					<NavLink tabIndex={1} activeClassName='nav__link-active' className='nav__link' to='/home'>
 						<Logo src={Home} />
 						Home
 					</NavLink>
-					<NavLink activeClassName='nav__link-active' className='nav__link' to='/albums'>
+					<NavLink
+						tabIndex={2}
+						activeClassName='nav__link-active'
+						className='nav__link'
+						to='/albums'
+					>
 						<Logo src={Book} />
 						Albumes
 					</NavLink>
-					<NavLink activeClassName='nav__link-active' className='nav__link' to='/artists'>
+					<NavLink
+						tabIndex={3}
+						activeClassName='nav__link-active'
+						className='nav__link'
+						to='/artists'
+					>
 						<Logo src={Ratio} />
 						Artista
 					</NavLink>
 				</div>
-				<div className='nav__center'>{children}</div>
+				<div tabIndex={4} className='nav__center'>
+					{children}
+				</div>
 				<div className='nav__right'>
-					<button className='nav__right-logout' onClick={logout}>
+					<button tabIndex={5} className='nav__right-logout' onClick={logout}>
 						Salir
 					</button>
 				</div>
--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,38 +1,34 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { FetchTokenAction } from '../../redux/actions'
 import Logo from 'components/logo'
 import Spotify from 'assets/img/logotipo.svg'
-import { Redirect } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { FetchTokenAction } from '../../redux/actions'
 import './styles.scss'
 
 const Login = () => {
-	const state = useSelector(state => state)
-
-	const {
-		reducerLogin: { token }
-	} = state
-
+	const token = useSelector(state => state.reducerLogin.token)
 	const dispatch = useDispatch()
+	const history = useHistory()
 
 	const login = () => dispatch(FetchTokenAction())
 
+	useLayoutEffect(() => {
+		token && history.push('/home')
+	}, [token, history])
+
 	return (
 		<>
-			{!token ? (
-				<div className='container'>
-					<div className='container-login'>
-						<h1 className='container-login__title'>
-							<Logo src={Spotify} /> Accesar a SpotifyClon
-						</h1>
-						<button className='container-login__button' onClick={login}>
-							Ingresar
-						</button>
-					</div>
+			<div tabIndex={1} className='container'>
+				<div tabIndex={2} className='container-login'>
+					<h1 tabIndex={3} className='container-login__title'>
+						<Logo src={Spotify} /> Accesar a SpotifyClon
+					</h1>
+					<button tabIndex={4} className='container-login__button' onClick={login}>
+						Ingresar
+					</button>
 				</div>
-			) : (
-				<Redirect to='/home' />
-			)}
+			</div>
 		</>
 	)
 }
